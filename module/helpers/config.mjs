050c89f9286aabd80b17cf65a5b28b0560ed40de
ARoserProgramming <arroser04@gmail.com>
2025-07-05 00:32:57 +0200

    Initial Commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const PM_TTRPG = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+PM_TTRPG.abilities = {
+  str: 'PM_TTRPG.Ability.Str.long',
+  dex: 'PM_TTRPG.Ability.Dex.long',
+  con: 'PM_TTRPG.Ability.Con.long',
+  int: 'PM_TTRPG.Ability.Int.long',
+  wis: 'PM_TTRPG.Ability.Wis.long',
+  cha: 'PM_TTRPG.Ability.Cha.long',
+};
+
+PM_TTRPG.abilityAbbreviations = {
+  str: 'PM_TTRPG.Ability.Str.abbr',
+  dex: 'PM_TTRPG.Ability.Dex.abbr',
+  con: 'PM_TTRPG.Ability.Con.abbr',
+  int: 'PM_TTRPG.Ability.Int.abbr',
+  wis: 'PM_TTRPG.Ability.Wis.abbr',
+  cha: 'PM_TTRPG.Ability.Cha.abbr',
+};
