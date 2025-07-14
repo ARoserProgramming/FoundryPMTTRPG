f128fdc9c009a4580db4b0bb1f09dc78f00c9ce9
ARoserProgramming <arroser04@gmail.com>
2025-07-15 01:00:09 +0200

    schema changes

--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -52,11 +52,16 @@ export default class PMTTRPGActorBase extends PMTTRPGDataModel {
         this.attack_modifier = this.rank;
         this.evade_modifier = this.abilities.ins.value;
         this.block_modifier = this.abilities.tmp.value;
+        this.tool_slots = {
+            initial: 4,
+            max: 4,
+        };
         this.equipment_rank_limit = this.rank + 1
         this.stagger_threshold.max = 20 + (this.abilities.chr.value * 4) + (this.rank * 4),
         this.health_points.max = 72 + (this.abilities.ftd.value * 8) + (this.rank * 8),
         this.light.max= 3 + this.rank;
-        };
+
+    }
     async _preCreate(data, options, user) {
         const allowed = await super._preCreate(data, options, user);
         if (allowed === false) return false;
