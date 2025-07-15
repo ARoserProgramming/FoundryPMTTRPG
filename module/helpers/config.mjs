98846d8bbed56a0e6f7de7ef99e12cb6c60aa321
mrosdom <mrosdom1504@g.educaand.es>
2025-07-16 00:09:42 +0200

    Traductions

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -5,83 +5,83 @@ export const PMTTRPG = {};
  * @type {Object}
  */
 PMTTRPG.abilities = {
-  ftd: 'PMTTRPG.Ability.Ftd.long',
-  prd: 'PMTTRPG.Ability.Prd.long',
-  jst: 'PMTTRPG.Ability.Jst.long',
-  chr: 'PMTTRPG.Ability.Chr.long',
-  ins: 'PMTTRPG.Ability.Ins.long',
-  tmp: 'PMTTRPG.Ability.Tmp.long',
+    ftd: 'PMTTRPG.Ability.Ftd.long',
+    prd: 'PMTTRPG.Ability.Prd.long',
+    jst: 'PMTTRPG.Ability.Jst.long',
+    chr: 'PMTTRPG.Ability.Chr.long',
+    ins: 'PMTTRPG.Ability.Ins.long',
+    tmp: 'PMTTRPG.Ability.Tmp.long',
 };
 
 PMTTRPG.abilityAbbreviations = {
-  ftd: 'PMTTRPG.Ability.Ftd.long',
-  prd: 'PMTTRPG.Ability.Prd.long',
-  jst: 'PMTTRPG.Ability.Jst.long',
-  chr: 'PMTTRPG.Ability.Chr.long',
-  ins: 'PMTTRPG.Ability.Ins.long',
-  tmp: 'PMTTRPG.Ability.Tmp.long',
+    ftd: 'PMTTRPG.Ability.Ftd.long',
+    prd: 'PMTTRPG.Ability.Prd.long',
+    jst: 'PMTTRPG.Ability.Jst.long',
+    chr: 'PMTTRPG.Ability.Chr.long',
+    ins: 'PMTTRPG.Ability.Ins.long',
+    tmp: 'PMTTRPG.Ability.Tmp.long',
 };
 PMTTRPG.damageTypes = {
- slash: {
-   label: 'PMTTRPG.DAMAGE_TYPE.Slash',
-    img: 'systems/pmttrpg/assets/imgs/damage/slash.svg',
+    slash: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Slash',
+        img: 'systems/pmttrpg/assets/imgs/conditions/slash.svg',
     },
     pierce: {
-    label: 'PMTTRPG.DAMAGE_TYPE.Pierce',
-    img: 'systems/pmttrpg/assets/imgs/damage/pierce.svg',
+        label: 'PMTTRPG.DAMAGE_TYPE.Pierce',
+        img: 'systems/pmttrpg/assets/imgs/conditions/pierce.svg',
     },
-  blunt: {
-    label: 'PMTTRPG.DAMAGE_TYPE.Blunt',
-    img: 'systems/pmttrpg/assets/imgs/damage/blunt.svg',
-  },
+    blunt: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Blunt',
+        img: 'systems/pmttrpg/assets/imgs/conditions/blunt.svg',
+    },
     burn: {
         label: 'PMTTRPG.DAMAGE_TYPE.Burn',
-        img: 'systems/pmttrpg/assets/imgs/damage/burn.svg',
+        img: 'systems/pmttrpg/assets/imgs/conditions/burn.svg',
     },
-  sinking: {
-    label: 'PMTTRPG.DAMAGE_TYPE.Sinking',
-    img: 'systems/pmttrpg/assets/imgs/damage/sinking.svg',
-  },
-  rupture: {
-    label: 'PMTTRPG.DAMAGE_TYPE.Rupture',
-    img: 'systems/pmttrpg/assets/imgs/damage/rupture.svg',
-  },
-  frostbite: {
-    label: 'PMTTRPG.DAMAGE_TYPE.Frostbite',
-    img: 'systems/pmttrpg/assets/imgs/damage/frostbite.svg',
-  },
-  bleed:{
-    label: 'PMTTRPG.DAMAGE_TYPE.Bleed',
-    img: 'systems/pmttrpg/assets/imgs/damage/bleed.svg',
-  },
-  tremor:{
-    label: 'PMTTRPG.DAMAGE_TYPE.Tremor',
-    img: 'systems/pmttrpg/assets/imgs/damage/tremor.svg',
-  },
-  critical:{
-    label: 'PMTTRPG.DAMAGE_TYPE.Critical',
-    img: 'systems/pmttrpg/assets/imgs/damage/critical.svg',
-  },
-  devastating:{
-   label: 'PMTTRPG.DAMAGE_TYPE.Devastating',
-    img: 'systems/pmttrpg/assets/imgs/damage/devastating.svg',
- },
-  physical:{
-    label: 'PMTTRPG.DAMAGE_TYPE.Physical',
-    img: 'systems/pmttrpg/assets/imgs/damage/physical.svg',
-  },
-  force:{
-    label: 'PMTTRPG.DAMAGE_TYPE.Force',
-    img: 'systems/pmttrpg/assets/imgs/damage/force.svg',
-  },
-  falling:{
-    label: 'PMTTRPG.DAMAGE_TYPE.Falling',
-    img: 'systems/pmttrpg/assets/imgs/damage/falling.svg',
-  }
+    sinking: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Sinking',
+        img: 'systems/pmttrpg/assets/imgs/conditions/sinking.svg',
+    },
+    rupture: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Rupture',
+        img: 'systems/pmttrpg/assets/imgs/conditions/rupture.svg',
+    },
+    frostbite: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Frostbite',
+        img: 'systems/pmttrpg/assets/imgs/conditions/frostbite.svg',
+    },
+    bleed: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Bleed',
+        img: 'systems/pmttrpg/assets/imgs/conditions/bleed.svg',
+    },
+    tremor: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Tremor',
+        img: 'systems/pmttrpg/assets/imgs/conditions/tremor.svg',
+    },
+    critical: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Critical',
+        img: 'systems/pmttrpg/assets/imgs/conditions/critical.svg',
+    },
+    devastating: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Devastating',
+        img: 'systems/pmttrpg/assets/imgs/conditions/devastating.svg',
+    },
+    physical: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Physical',
+        img: 'systems/pmttrpg/assets/imgs/conditions/physical.svg',
+    },
+    force: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Force',
+        img: 'systems/pmttrpg/assets/imgs/conditions/force.svg',
+    },
+    falling: {
+        label: 'PMTTRPG.DAMAGE_TYPE.Falling',
+        img: 'systems/pmttrpg/assets/imgs/conditions/falling.svg',
+    }
 }
 PMTTRPG.healingTypes = {
-  value: "PMTTRPG.HealingType.Value",
-  temporary:"PMTTRPG.HealingType.Temporary",
+    value: "PMTTRPG.HealingType.Value",
+    temporary: "PMTTRPG.HealingType.Temporary",
 }
 PMTTRPG.conditions = {
     burn: {
@@ -104,11 +104,11 @@ PMTTRPG.conditions = {
         name: 'PMTTRPG.Condition.Tremor',
         img: 'systems/pmttrpg/assets/imgs/conditions/tremor.svg',
     },
-    sinking:{
+    sinking: {
         name: 'PMTTRPG.Condition.Sinking',
         img: 'systems/pmttrpg/assets/imgs/conditions/sinking.svg',
     },
-    poise : {
+    poise: {
         name: 'PMTTRPG.Condition.Poise',
         img: 'systems/pmttrpg/assets/imgs/conditions/poise.svg',
     },
@@ -116,12 +116,12 @@ PMTTRPG.conditions = {
         name: 'PMTTRPG.Condition.Critical',
         img: 'systems/pmttrpg/assets/imgs/conditions/critical.svg',
     },
-    ruin :{
+    ruin: {
         name: 'PMTTRPG.Condition.Ruin',
         img: 'systems/pmttrpg/assets/imgs/conditions/ruin.svg',
     },
     devastation: {
-        name: 'PMTTRPG.Condition.Devastating',
+        name: 'PMTTRPG.Condition.Devastation',
         img: 'systems/pmttrpg/assets/imgs/conditions/devastation.svg',
     },
     paralysis: {
@@ -135,6 +135,10 @@ PMTTRPG.conditions = {
     stagger_protection: {
         name: 'PMTTRPG.Condition.StaggerProtection',
         img: 'systems/pmttrpg/assets/imgs/conditions/stagger_protection.svg',
+    },
+    stagger_fragile:{
+        name: 'PMTTRPG.Condition.StaggerFragile',
+        img: 'systems/pmttrpg/assets/imgs/conditions/stagger_fragile.svg',
     },
     slash_protection: {
         name: 'PMTTRPG.Condition.SlashProtection',
@@ -164,19 +168,19 @@ PMTTRPG.conditions = {
         name: 'PMTTRPG.Condition.BluntFragile',
         img: 'systems/pmttrpg/assets/imgs/conditions/blunt_fragile.svg',
     },
-    strength:{
+    strength: {
         name: 'PMTTRPG.Condition.Strength',
         img: 'systems/pmttrpg/assets/imgs/conditions/strength.svg',
     },
-    feeble:{
+    feeble: {
         name: 'PMTTRPG.Condition.Feeble',
         img: 'systems/pmttrpg/assets/imgs/conditions/feeble.svg',
     },
-    endurance:{
+    endurance: {
         name: 'PMTTRPG.Condition.Endurance',
         img: 'systems/pmttrpg/assets/imgs/conditions/endurance.svg',
     },
-    disarm:{
+    disarm: {
         name: 'PMTTRPG.Condition.Disarm',
         img: 'systems/pmttrpg/assets/imgs/conditions/disarm.svg',
     },
@@ -184,11 +188,11 @@ PMTTRPG.conditions = {
         name: 'PMTTRPG.Condition.Haste',
         img: 'systems/pmttrpg/assets/imgs/conditions/haste.svg',
     },
-    bind:{
+    bind: {
         name: 'PMTTRPG.Condition.Bind',
         img: 'systems/pmttrpg/assets/imgs/conditions/bind.svg',
     },
-    smoke:{
+    smoke: {
         name: 'PMTTRPG.Condition.Smoke',
         img: 'systems/pmttrpg/assets/imgs/conditions/smoke.svg',
     },
@@ -196,11 +200,11 @@ PMTTRPG.conditions = {
         name: 'PMTTRPG.Condition.Charge',
         img: 'systems/pmttrpg/assets/imgs/conditions/charge.svg',
     },
-    charge_barrier:{
+    charge_barrier: {
         name: 'PMTTRPG.Condition.ChargeBarrier',
         img: 'systems/pmttrpg/assets/imgs/conditions/charge_barrier.svg',
     },
-    overcharge:{
+    overcharge: {
         name: 'PMTTRPG.Condition.Overcharge',
         img: 'systems/pmttrpg/assets/imgs/conditions/overcharge.svg',
     },
@@ -208,51 +212,51 @@ PMTTRPG.conditions = {
         name: 'PMTTRPG.Condition.Mark',
         img: 'systems/pmttrpg/assets/imgs/conditions/mark.svg',
     },
-    combo:{
+    combo: {
         name: 'PMTTRPG.Condition.Combo',
         img: 'systems/pmttrpg/assets/imgs/conditions/combo.svg',
     },
-    staggered:{
+    staggered: {
         name: 'PMTTRPG.Condition.Staggered',
         img: 'systems/pmttrpg/assets/imgs/conditions/staggered.svg',
     },
-    panic:{
+    panic: {
         name: 'PMTTRPG.Condition.Panic',
         img: 'systems/pmttrpg/assets/imgs/conditions/panic.svg',
     },
-    defeated:{
+    defeated: {
         name: 'PMTTRPG.Condition.Defeated',
         img: 'systems/pmttrpg/assets/imgs/conditions/defeated.svg',
     },
-    alert:{
+    alert: {
         name: 'PMTTRPG.Condition.Alert',
         img: 'systems/pmttrpg/assets/imgs/conditions/alert.svg',
     },
-    hidden:{
+    hidden: {
         name: 'PMTTRPG.Condition.Hidden',
         img: 'systems/pmttrpg/assets/imgs/conditions/hidden.svg',
     },
-    grappling_grappler:{
+    grappling_grappler: {
         name: 'PMTTRPG.Condition.GrapplingGrappler',
         img: 'systems/pmttrpg/assets/imgs/conditions/grappling_grappler.svg',
     },
-    grappling_target:{
+    grappling_target: {
         name: 'PMTTRPG.Condition.GrapplingTarget',
         img: 'systems/pmttrpg/assets/imgs/conditions/grappling_target.svg',
     },
-    scarred:{
-        name: 'PMTTRPG.Condition.Scared',
+    scarred: {
+        name: 'PMTTRPG.Condition.Scarred',
         img: 'systems/pmttrpg/assets/imgs/conditions/scared.svg',
     },
-    limb_loss:{
+    limb_loss: {
         name: 'PMTTRPG.Condition.LimbLoss',
         img: 'systems/pmttrpg/assets/imgs/conditions/limb_loss.svg',
     },
-    limb_loss_arm:{
+    limb_loss_arm: {
         name: 'PMTTRPG.Condition.LimbLossArm',
         img: 'systems/pmttrpg/assets/imgs/conditions/limb_loss_arm.svg',
     },
-    limb_loss_leg:{
+    limb_loss_leg: {
         name: 'PMTTRPG.Condition.LimbLossLeg',
         img: 'systems/pmttrpg/assets/imgs/conditions/limb_loss_leg.svg',
     },
