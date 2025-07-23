63ef78260e1cdb07ccddb267bb60589110adcceb
mrosdom <mrosdom1504@g.educaand.es>
2025-07-23 19:30:52 +0200

    character sheet

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -105,168 +105,195 @@ PMTTRPG.conditions = {
         name: 'PMTTRPG.Condition.Burn',
         img: 'systems/pmttrpg/assets/imgs/conditions/burn.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => Math.floor(potency / 2)
+        potencyReduceFormula: (potency) => Math.floor(potency / 2),
+        damaging: true
     },
     frostbite: {
         name: 'PMTTRPG.Condition.Frostbite',
         img: 'systems/pmttrpg/assets/imgs/conditions/frostbite.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => Math.floor(potency / 2)
+        potencyReduceFormula: (potency) => Math.floor(potency / 2),
+        damaging: true
     },
     bleed: {
         name: 'PMTTRPG.Condition.Bleed',
         img: 'systems/pmttrpg/assets/imgs/conditions/bleed.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => Math.floor(potency / 2)
+        potencyReduceFormula: (potency) => Math.floor(potency / 2),
+        damaging: true
     },
     rupture: {
         name: 'PMTTRPG.Condition.Rupture',
         img: 'systems/pmttrpg/assets/imgs/conditions/rupture.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: true
     },
     tremor: {
         name: 'PMTTRPG.Condition.Tremor',
         img: 'systems/pmttrpg/assets/imgs/conditions/tremor.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: true
 
     },
     sinking: {
         name: 'PMTTRPG.Condition.Sinking',
         img: 'systems/pmttrpg/assets/imgs/conditions/sinking.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: true
     },
     poise: {
         name: 'PMTTRPG.Condition.Poise',
         img: 'systems/pmttrpg/assets/imgs/conditions/poise.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     critical: {
         name: 'PMTTRPG.Condition.Critical',
         img: 'systems/pmttrpg/assets/imgs/conditions/critical.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     ruin: {
         name: 'PMTTRPG.Condition.Ruin',
         img: 'systems/pmttrpg/assets/imgs/conditions/ruin.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     devastation: {
         name: 'PMTTRPG.Condition.Devastation',
         img: 'systems/pmttrpg/assets/imgs/conditions/devastation.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     paralysis: {
         name: 'PMTTRPG.Condition.Paralysis',
         img: 'systems/pmttrpg/assets/imgs/conditions/paralysis.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     protection: {
         name: 'PMTTRPG.Condition.Protection',
         img: 'systems/pmttrpg/assets/imgs/conditions/protection.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     stagger_protection: {
         name: 'PMTTRPG.Condition.StaggerProtection',
         img: 'systems/pmttrpg/assets/imgs/conditions/stagger_protection.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     stagger_fragile: {
         name: 'PMTTRPG.Condition.StaggerFragile',
         img: 'systems/pmttrpg/assets/imgs/conditions/stagger_fragile.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     slash_protection: {
         name: 'PMTTRPG.Condition.SlashProtection',
         img: 'systems/pmttrpg/assets/imgs/conditions/slash_protection.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     pierce_protection: {
         name: 'PMTTRPG.Condition.PierceProtection',
         img: 'systems/pmttrpg/assets/imgs/conditions/pierce_protection.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     blunt_protection: {
         name: 'PMTTRPG.Condition.BluntProtection',
         img: 'systems/pmttrpg/assets/imgs/conditions/blunt_protection.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     fragile: {
         name: 'PMTTRPG.Condition.Fragile',
         img: 'systems/pmttrpg/assets/imgs/conditions/fragile.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     slash_fragile: {
         name: 'PMTTRPG.Condition.SlashFragile',
         img: 'systems/pmttrpg/assets/imgs/conditions/slash_fragile.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     pierce_fragile: {
         name: 'PMTTRPG.Condition.PierceFragile',
         img: 'systems/pmttrpg/assets/imgs/conditions/pierce_fragile.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     blunt_fragile: {
         name: 'PMTTRPG.Condition.BluntFragile',
         img: 'systems/pmttrpg/assets/imgs/conditions/blunt_fragile.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     strength: {
         name: 'PMTTRPG.Condition.Strength',
         img: 'systems/pmttrpg/assets/imgs/conditions/strength.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     feeble: {
         name: 'PMTTRPG.Condition.Feeble',
         img: 'systems/pmttrpg/assets/imgs/conditions/feeble.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
 
     },
     endurance: {
         name: 'PMTTRPG.Condition.Endurance',
         img: 'systems/pmttrpg/assets/imgs/conditions/endurance.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
 
     },
     disarm: {
         name: 'PMTTRPG.Condition.Disarm',
         img: 'systems/pmttrpg/assets/imgs/conditions/disarm.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
 
     },
     haste: {
         name: 'PMTTRPG.Condition.Haste',
         img: 'systems/pmttrpg/assets/imgs/conditions/haste.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
 
     },
     bind: {
         name: 'PMTTRPG.Condition.Bind',
         img: 'systems/pmttrpg/assets/imgs/conditions/bind.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
 
     },
     smoke: {
@@ -276,100 +303,115 @@ PMTTRPG.conditions = {
         potencyReduceFormula: (potency) => {
             if (potency > 10) return Math.max(0, potency - 4);
             return Math.max(0, potency - 2);
-        }
+        },
+        damaging: false
     },
     charge: {
         name: 'PMTTRPG.Condition.Charge',
         img: 'systems/pmttrpg/assets/imgs/conditions/charge.png',
         potency: {value: 1, min: 0, max: 999},
-
+        damaging: false
     },
     charge_barrier: {
         name: 'PMTTRPG.Condition.ChargeBarrier',
         img: 'systems/pmttrpg/assets/imgs/conditions/charge_barrier.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
 
     },
     overcharge: {
         name: 'PMTTRPG.Condition.Overcharge',
         img: 'systems/pmttrpg/assets/imgs/conditions/overcharge.png',
-        potency: {value: 1, min: 0, max: 999}
+        potency: {value: 1, min: 0, max: 999},
+        damaging: false
     },
     mark: {
         name: 'PMTTRPG.Condition.Mark',
         img: 'systems/pmttrpg/assets/imgs/conditions/mark.png',
-        potency: {value: 1, min: 0, max: 99}
+        potency: {value: 1, min: 0, max: 99},
+        damaging: false
     },
     combo: {
         name: 'PMTTRPG.Condition.Combo',
         img: 'systems/pmttrpg/assets/imgs/conditions/combo.png',
         potency: {value: 1, min: 0, max: 99},
-        potencyReduceFormula: (potency) => Math.floor(potency / 2)
-
+        potencyReduceFormula: (potency) => Math.floor(potency / 2),
+        damaging: false
     },
     staggered: {
         name: 'PMTTRPG.Condition.Staggered',
         img: 'systems/pmttrpg/assets/imgs/conditions/staggered.png',
         potency: {value: 1, min: 0, max: 1},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     panic: {
         name: 'PMTTRPG.Condition.Panic',
         img: 'systems/pmttrpg/assets/imgs/conditions/panic.png',
         potency: {value: 1, min: 0, max: 1},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     defeated: {
         name: 'PMTTRPG.Condition.Defeated',
         img: 'systems/pmttrpg/assets/imgs/conditions/defeated.png',
         potency: {value: 1, min: 0, max: 1},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     alert: {
         name: 'PMTTRPG.Condition.Alert',
         img: 'systems/pmttrpg/assets/imgs/conditions/alert.png',
         potency: {value: 1, min: 0, max: 1},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     hidden: {
         name: 'PMTTRPG.Condition.Hidden',
         img: 'systems/pmttrpg/assets/imgs/conditions/hidden.png',
         potency: {value: 1, min: 0, max: 1},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     grappling_grappler: {
         name: 'PMTTRPG.Condition.GrapplingGrappler',
         img: 'systems/pmttrpg/assets/imgs/conditions/grappling_grappler.png',
         potency: {value: 1, min: 0, max: 1},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     grappling_target: {
         name: 'PMTTRPG.Condition.GrapplingTarget',
         img: 'systems/pmttrpg/assets/imgs/conditions/grappling_target.png',
         potency: {value: 1, min: 0, max: 1},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     scarred: {
         name: 'PMTTRPG.Condition.Scarred',
         img: 'systems/pmttrpg/assets/imgs/conditions/scared.png',
         potency: {value: 1, min: 0, max: 1},
-        potencyReduceFormula: (potency) => 0
+        potencyReduceFormula: (potency) => 0,
+        damaging: false
     },
     limb_loss: {
         name: 'PMTTRPG.Condition.LimbLoss',
         img: 'systems/pmttrpg/assets/imgs/conditions/limb_loss.png',
-        potency: {value: 1, min: 0, max: 4}
+        potency: {value: 1, min: 0, max: 4},
+        damaging: false
     },
     limb_loss_arm: {
         name: 'PMTTRPG.Condition.LimbLossArm',
         img: 'systems/pmttrpg/assets/imgs/conditions/limb_loss_arm.png',
-        potency: {value: 1, min: 0, max: 2}
+        potency: {value: 1, min: 0, max: 2},
+        damaging: false
     },
     limb_loss_leg: {
         name: 'PMTTRPG.Condition.LimbLossLeg',
         img: 'systems/pmttrpg/assets/imgs/conditions/limb_loss_leg.png',
-        potency: {value: 1, min: 0, max: 2}
+        potency: {value: 1, min: 0, max: 2},
+        damaging: false
     }
 }
 PMTTRPG.effectEnd ={
