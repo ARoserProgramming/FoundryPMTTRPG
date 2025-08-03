235020ef3b750d8bc83f01b5aaf800f5020419d6
ARoserProgramming <arroser04@gmail.com>
2025-08-03 21:43:06 +0200

    temporal health/stagger added

--- module/data/item-item.mjs
+++ module/data/item-item.mjs
@@ -13,8 +13,8 @@ export default class PMTTRPGItem extends PMTTRPGItemBase {
     // Break down roll formula into three independent fields
     schema.roll = new fields.SchemaField({
       diceNum: new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 }),
-      diceSize: new fields.StringField({ initial: "d20" }),
-      diceBonus: new fields.StringField({ initial: "+@str.mod+ceil(@lvl / 2)" })
+      diceSize: new fields.StringField({ initial: "d12" }),
+      diceBonus: new fields.StringField({ initial: "0" })
     })
 
     schema.formula = new fields.StringField({ blank: true });
